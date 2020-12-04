09c092550cb7863f50d38c4afab0561b1b56cf94
Nuchana <nuchana.johnson@gmail.com>
2020-12-03 21:21:26 -0500

    fix virtual property in workout.js

--- models/Workout.js
+++ models/Workout.js
@@ -2,60 +2,77 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-    day: {
-        type: Date,
-        default: Date.now
+const WorkoutSchema = new Schema(
+    {
+        day: {
+            type: Date,
+            default: Date.now
+        },
+        exercise: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise type"
+                },
+
+
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter an exercise name"
+                },
+
+                duration: {
+                    type: Number,
+                    required: "Enter an exercise duration in minutes"
+
+                },
+
+                weight: {
+                    type: Number,
+                    default: 0
+                },
+                reps: {
+                    type: Number,
+                    default: 0
+
+                },
+                sets: {
+                    type: Number,
+                    default: 0
+                },
+                distance: {
+                    type: Number,
+                    default: 0
+                },
+
+
+
+            }
+        ]
     },
-    exercise: [{
-        type: {
-            type: String,
-            trim: true,
-            required: "Enter an exercise type"
+
+    {
+        toJSON: {
+            virtual: true
         },
-
-
-        name: {
-            type: String,
-            trim: true,
-            required: "Enter an exercise name"
-        },
-
-        duration: {
-            type: Number,
-            required: "Enter an exercise duration in minutes"
-
-        },
-
-        weight: {
-            type: Number,
-            default: 0
-        },
-        reps: {
-            type: Number,
-            default: 0
-
-        },
-        sets: {
-            type: Number,
-            default: 0
-        },
-        distance: {
-            type: Number,
-            default: 0
-        },
-
-
-
-    }],
-
-    totalDuration: {
-        type: Number,
-        default: 0,
     }
+);
 
-
+// adds a dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function () {
+    // "reduce" array of exercises down to just the sum of their durations
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
 });
+
+// totalDuration: {
+//     type: Number,
+//     default: 0,
+// }
+
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
 
