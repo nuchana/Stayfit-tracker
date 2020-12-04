202f5284ce50e03e3d379a45bd9d8752fcffd785
Nuchana <nuchana.johnson@gmail.com>
2020-12-03 23:07:47 -0500

    issue findbyidandupdate

--- routes/api.js
+++ routes/api.js
@@ -3,13 +3,15 @@ const Workout = require("../models");
 
 // create workout ID
 router.post("/api/workouts", function (req, res) {
-  Workout.create(req.body)
+  // Workout.create(req.body)
+  Workout.create({})
     .then(function (dbWorkout) {
       res.json(dbWorkout);
     })
     .catch(err => {
       res.json(err);
     });
+    
 });
 
 // GET route for getting workouts
@@ -21,18 +23,47 @@ router.get("/api/workouts", function (req, res) {
     });
 });
 
-// Get route for retrieving a current workout
-router.put("/api/workouts/:id", function (req, res) {
-  Workout.findOne({
-    where: {
-      id: req.params.id
-    }
-  }).then(function (dbWorkout) {
-    console.log(dbWorkout);
-    res.json(dbWorkout);
-  });
+// Get route for retrieving a sepecific workout 
+
+router.put("/api/workouts/:id", function ({ body, params }, res) {
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    // "runValidators" will ensure new exercises meet our schema requirements
+    { new: true, runValidators: true }
+  )
+    .then(function (dbWorkout) {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
+// router.put("/api/workouts/:id", function (req, res) {
+//   Workout.findOne({
+//     where: {
+//       id: req.params.id
+//     }
+//   }).then(function (dbWorkout) {
+//     console.log(dbWorkout);
+//     res.json(dbWorkout);
+//   });
+// });
+
+// PUT route for updating posts
+// router.put("/api/workouts", function (req, res) {
+//   Workout.update(
+//     req.body,
+//     {
+//       where: {
+//         id: req.body.id
+//       }
+//     }).then(function (dbWorkout) {
+//       res.json(dbWorkout);
+//     });
+// });
 
 
 // DELETE route for deleting posts
@@ -46,32 +77,18 @@ router.delete("/api/workouts/:id", function (req, res) {
   });
 });
 
-// PUT route for updating posts
-router.put("/api/workouts", function (req, res) {
-  Workout.update(
-    req.body,
-    {
-      where: {
-        id: req.body.id
-      }
-    }).then(function (dbWorkout) {
-      res.json(dbWorkout);
-    });
-});
-
-
 
 router.get("/api/workouts/range", (req, res) => {
 
   Workout.find({}).limit(7)
-  .then(dbWorkout => {
-    console.log("ALL WORKOUTS");
-    console.log(dbWorkout);
+    .then(dbWorkout => {
+      console.log("ALL WORKOUTS");
+      console.log(dbWorkout);
 
-    res.json(dbWorkout);
-  }).catch(err => {
-    res.json(err);
-  });
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.json(err);
+    });
 
 });
 
