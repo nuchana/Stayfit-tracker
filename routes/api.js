bb745863e8df73eef3359c01d564eb7ef25d174f
Nuchana <nuchana.johnson@gmail.com>
2020-12-05 00:15:43 -0500

    fix capital Workout.js for heroku problem

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
+const Workout = require("../models/Workout.js");
 //const db = require('../models')
 //db.Workout
 //const Workout = require('../models/Workout')
